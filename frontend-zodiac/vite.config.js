b572bb3a942ac9724d6880484200688791c638a5
Tony Luong <tonyluong@Tonys-MacBook-Pro.local>
2025-06-07 09:17:43 -0700

    fix:update base path for GitHub Pages deployment

--- frontend-zodiac/vite.config.js
+++ frontend-zodiac/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repoName = 'CungHoangDaoWebApp';
+const repoName = 'CungHoangDaoAIWebApp';
 
 export default defineConfig({
   plugins: [react()],
